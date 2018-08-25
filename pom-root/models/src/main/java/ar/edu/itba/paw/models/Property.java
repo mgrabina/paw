f1a566d1e393285974904eb489953f3df73660d9
mgrabina <mgrabina@itba.edu.ar>
2018-08-25 16:25:04 -0300

    User and Property Models

---
+++ pom-root/models/src/main/java/ar/edu/itba/paw/models/Property.java
@@ -0,0 +1,134 @@
+package ar.edu.itba.paw.models;
+
+public class Property {
+    private Integer id;
+    private String street;
+    private Integer number;
+    private Integer floor;
+    private String apartment;
+    private enum type{
+        house, apartment, duplex, office, land
+    };
+    private Integer userId;
+    private Long price;
+    //Images in Imagesxproperty relation
+    private Integer coveredArea;    //In quarter meters
+    private Integer totalArea;    //In quarter meters
+    private Integer rooms;
+    private Integer baths;
+    private Boolean garage;
+    private Integer taxPrice;
+
+    public Property(String street, Integer number, Integer floor, String apartment, Integer userId, Long price) {
+        this.street = street;
+        this.number = number;
+        this.floor = floor;
+        this.apartment = apartment;
+        this.userId = userId;
+        this.price = price;
+    }
+
+    public Property(String street, Integer number, Integer userId, Long price) {
+        this.street = street;
+        this.number = number;
+        this.userId = userId;
+        this.price = price;
+    }
+
+    public Property(String street, Integer number, Integer floor, String apartment, Integer userId, Long price, Integer coveredArea, Integer totalArea, Integer rooms, Integer baths, Boolean garage, Integer taxPrice) {
+
+        this.street = street;
+        this.number = number;
+        this.floor = floor;
+        this.apartment = apartment;
+        this.userId = userId;
+        this.price = price;
+        this.coveredArea = coveredArea;
+        this.totalArea = totalArea;
+        this.rooms = rooms;
+        this.baths = baths;
+        this.garage = garage;
+        this.taxPrice = taxPrice;
+    }
+
+    public void setPrice(Long price) {
+        this.price = price;
+    }
+
+    public void setCoveredArea(Integer coveredArea) {
+        this.coveredArea = coveredArea;
+    }
+
+    public void setTotalArea(Integer totalArea) {
+        this.totalArea = totalArea;
+    }
+
+    public void setRooms(Integer rooms) {
+        this.rooms = rooms;
+    }
+
+    public void setBaths(Integer baths) {
+        this.baths = baths;
+    }
+
+    public void setGarage(Boolean garage) {
+        this.garage = garage;
+    }
+
+    public void setTaxPrice(Integer taxPrice) {
+        this.taxPrice = taxPrice;
+    }
+
+    public Integer getId() {
+
+        return id;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public Integer getFloor() {
+        return floor;
+    }
+
+    public String getApartment() {
+        return apartment;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public Integer getCoveredArea() {
+        return coveredArea;
+    }
+
+    public Integer getTotalArea() {
+        return totalArea;
+    }
+
+    public Integer getRooms() {
+        return rooms;
+    }
+
+    public Integer getBaths() {
+        return baths;
+    }
+
+    public Boolean getGarage() {
+        return garage;
+    }
+
+    public Integer getTaxPrice() {
+        return taxPrice;
+    }
+}
--- pom-root/models/src/main/java/ar/edu/itba/paw/models/User.java
+++ pom-root/models/src/main/java/ar/edu/itba/paw/models/User.java
@@ -1,22 +1,57 @@
 package ar.edu.itba.paw.models;
 
 public class User {
-	
-	private String name;
+
 	private int id;
-	public User(String name,int id) {
+	private String name;
+	private String surname;
+	private String password;
+	private String phone;
+	private String mail;
+	private String imageSrc;
+
+	public User(String name, String surname, String password, String phone, String mail, String imageSrc) {
 		this.name = name;
-		this.id =id;
+		this.surname = surname;
+		this.password = password;
+		this.phone = phone;
+		this.mail = mail;
+		this.imageSrc = imageSrc;
+	}
+
+	public int getId() {
+		return id;
 	}
 
 	public String getName() {
 		return name;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public String getSurname() {
+		return surname;
 	}
-	
-	
 
+	public String getPassword() {
+		return password;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public String getImageSrc() {
+		return imageSrc;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setImageSrc(String imageSrc) {
+		this.imageSrc = imageSrc;
+	}
 }
