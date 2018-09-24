133477a92fc62909571fdd15d285355d968ed005
Martin <martinnagelberg@gmail.com>
2018-09-24 05:54:20 -0300

    autocomplete for address

--- pom-root/webapp/src/main/webapp/resources/js/register_property.js
+++ pom-root/webapp/src/main/webapp/resources/js/register_property.js
@@ -5,6 +5,9 @@ var stepOne = [false, false, false, false, false, false, false, false, false, fa
 var stepTwo = [false];
 var imageCarousel;
 
+var placeSearch, autocomplete;
+
+
 $(document).ready(function(){
 
 	var title, description, opType, delType, price, street, number, floor, apartment, neighborhood, propType, cArea, tArea, tPrice, rooms, baths, garage;
@@ -413,4 +416,45 @@ function checkField(field, inputData, rules) {
 
 }
 
+function initGoogleMapsAutocomplete() {
+	// Create the autocomplete object, restricting the search to geographical
+	// location types.
+	autocomplete = new google.maps.places.Autocomplete(
+	    /** @type {!HTMLInputElement} */(document.getElementById('p-street')),
+	    {types: ['geocode']});
+
+	autocomplete.setComponentRestrictions(
+            {'country': ['ar']});
+
+	// When the user selects an address from the dropdown, populate the address
+	// fields in the form.
+	autocomplete.addListener('place_changed', fillInAddress);
+}
+
+
+function fillInAddress() {
+	// Get the place details from the autocomplete object.
+	var place = autocomplete.getPlace();
+
+	var n = place.address_components[2].short_name;
+	neighborhood.val(n);
+
+}
+
+function geolocate() {
+	console.log("asd");
+	  navigator.geolocation.getCurrentPosition(function(position) {
+	  	console.log("recibi");
+	    var geolocation = {
+	      lat: position.coords.latitude,
+	      lng: position.coords.longitude
+	    };
+	    var circle = new google.maps.Circle({
+	      center: geolocation,
+	      radius: position.coords.accuracy
+	    });
+	    autocomplete.setBounds(circle.getBounds());
+	  });
+}
+   
 
