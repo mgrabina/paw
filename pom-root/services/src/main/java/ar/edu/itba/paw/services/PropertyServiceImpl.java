78a83826d061b036a61480b7dff7762cae75763c
mgrabina <mgrabina@itba.edu.ar>
2018-09-24 03:10:15 -0300

    fix for more than one street/neighborhood

--- pom-root/services/src/main/java/ar/edu/itba/paw/services/PropertyServiceImpl.java
+++ pom-root/services/src/main/java/ar/edu/itba/paw/services/PropertyServiceImpl.java
@@ -69,8 +69,12 @@ public class PropertyServiceImpl implements PropertyService {
 
 	public Long createProperty(String street, Integer number, Integer floor, String apartment, String neighborhood, OperationType operationType, PropertyType type, User publisherUser, Long price, Integer coveredArea, Integer totalArea, Integer rooms, Integer baths, Boolean garage, Integer taxPrice, String adMessage, String adDescription, Boolean inmediateDelivery) {
 	    Map<String, Integer> tags = new HashMap<String, Integer>();
-	    tags.put(street.trim().toLowerCase(), FilterType.street.ordinal());
-	    tags.put(neighborhood.trim().toLowerCase(), FilterType.neighborhood.ordinal());
+		Arrays.stream(street.trim().toLowerCase().split("\\s+")).forEach(
+				word -> tags.put( word, FilterType.street.ordinal())
+		);
+		Arrays.stream(neighborhood.trim().toLowerCase().split("\\s+")).forEach(
+				word -> tags.put( word, FilterType.neighborhood.ordinal())
+		);
 	    tags.put(operationType.toString(), FilterType.operationType.ordinal());
 	    tags.put(type.toString(), FilterType.type.ordinal());
 	    tags.put(rooms.toString() + "_rooms", FilterType.rooms.ordinal());
